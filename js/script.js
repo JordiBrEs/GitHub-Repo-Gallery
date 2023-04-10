49edb62a6a93ec4707a2aec5a59cfaedc36171bc
jordi_brunet_espuela <jordi.brunetespuela@gmail.com>
2023-04-10 09:18:56 -0400

    Repositories fetched

--- js/script.js
+++ js/script.js
@@ -1,35 +1,49 @@
 // div overView where my profile info will appear
 const overview = document.querySelector(".overview");
+const repoList = document.querySelector(".repo-list");
 
 const username = "jordiBrEs";
 
 const userInformation = async function() {
     const showRequest = await fetch(`https://api.github.com/users/${username}`);
-    const infoRequested = await showRequest.json();
+    const infoData = await showRequest.json();
     
-    // console.log(infoRequested);
+    // console.log(infoData);
 
-    informationDisplay(infoRequested);
+    informationDisplay(infoData);
 }
 
 userInformation();
 
 
-const informationDisplay = function(infoRequested){
+const informationDisplay = function(infoData){
     const div = document.createElement("div");
 
     div.classList.add("user-info");
 
-    div.innerHTML = `<figure> <img alt="user avatar" src=${infoRequested.avatar_url} /> </figure> <div> <p> <strong>Name:</strong> ${infoRequested.name} </p> <p><strong>Bio:</strong> ${infoRequested.bio}</p><p><strong>Location:</strong> ${infoRequested.location}</p><p><strong>Number of public repos:</strong> ${infoRequested.public_repos}</p></div>`;
+    div.innerHTML = `<figure> <img alt="user avatar" src=${infoData.avatar_url} /> </figure> <div> <p> <strong>Name:</strong> ${infoData.name} </p> <p><strong>Bio:</strong> ${infoData.bio}</p><p><strong>Location:</strong> ${infoData.location}</p><p><strong>Number of public repos:</strong> ${infoData.public_repos}</p></div>`;
 
     overview.append(div);
+    myRepos();
 
 };
 
+const myRepos= async function(){
+    const repositoryRequest = await fetch(`https://api.github.com/users/${username}/repos?sort=updated&per_page=100`);
+    const repositoryData = await repositoryRequest.json();
+    console.log(repositoryData); 
+    // - by calling this you can see the data on the console
+    displayRepos(repositoryData);
+}
 
+const displayRepos = function(repos){
+    for(const repo of repos) {
+        const listOfRepos = document.createElement("li");
+        listOfRepos.classList.add("repos");
+        listOfRepos.innerHTML = `<h3> ${repo.name} </h3>`
+        repoList.append(listOfRepos);
 
-// fetch(`https://api.github.com/users/${username}`)
-// .then(res => res.json())
-// .then(data => console.log(data));
+    }
+};
 
-
+// displayRepos(repositoryData);
